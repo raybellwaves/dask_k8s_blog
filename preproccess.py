0d902621619052545c246a6b5358a441bad98405
Ray Bell <rayjohnbell0@gmail.com>
2019-12-28 21:49:03 -0500

    Add files via upload

---
+++ preproccess.py
@@ -0,0 +1,5 @@
+# The file gs://dask-nyc-taxi/yellowtrip.parquet
+# comes from
+# https://github.com/TomAugspurger/pydata-nyc-2019-scalable-ml/blob/master/examples/data.ipynb
+#
+# although I have tested it for reproduction
